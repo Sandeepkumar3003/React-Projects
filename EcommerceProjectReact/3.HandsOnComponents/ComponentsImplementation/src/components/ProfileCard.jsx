c85ea6dc4682c4caab24011a6b683d81ea602e5e
Sandeepkumar <sandeepkumarrk1112@gmail.com>
2026-02-21 21:05:21 +0530

    Destructuring props

--- EcommerceProjectReact/3.HandsOnComponents/ComponentsImplementation/src/components/ProfileCard.jsx
+++ EcommerceProjectReact/3.HandsOnComponents/ComponentsImplementation/src/components/ProfileCard.jsx
@@ -1,14 +1,14 @@
 
 
-function ProfileCard(props){
+function ProfileCard({name,age,isMember}){
     // const name = "Alice";
-    const sum = props.num + 1;
+   
     return(
         <div>
-            <h3>Name: {props.name}</h3>
-            <h3>Age: {props.age}</h3>
-            <h3>Num: {sum}</h3>
-            <h3>Status: {props.isMember ? "Active Member" : "Guest"}</h3>
+            <h3>Name: {name}</h3>
+            <h3>Age: {age}</h3>
+        
+            <h3>Status: {isMember ? "Active Member" : "Guest"}</h3>
         </div>
     );
 }
