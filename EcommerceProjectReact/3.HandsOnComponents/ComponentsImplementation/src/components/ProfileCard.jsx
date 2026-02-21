f9c99799f5bcc6b7de1f27bd4c2bd5d3814248be
Sandeepkumar <sandeepkumarrk1112@gmail.com>
2026-02-21 20:57:15 +0530

    Adding Boolean conditions and displaying output

--- EcommerceProjectReact/3.HandsOnComponents/ComponentsImplementation/src/components/ProfileCard.jsx
+++ EcommerceProjectReact/3.HandsOnComponents/ComponentsImplementation/src/components/ProfileCard.jsx
@@ -8,6 +8,7 @@ function ProfileCard(props){
             <h3>Name: {props.name}</h3>
             <h3>Age: {props.age}</h3>
             <h3>Num: {sum}</h3>
+            <h3>Status: {props.isMember ? "Active Member" : "Guest"}</h3>
         </div>
     );
 }
