80ecfc008e52b1e95b071517e318620d2bbfcb75
Sandeepkumar <sandeepkumarrk1112@gmail.com>
2026-02-21 17:42:48 +0530

    Built basic React Portfolio Website

--- EcommerceProjectReact/2.BuildingPortfolioWebsiteUsingReact/PortfolioWebsite/src/App.jsx
+++ EcommerceProjectReact/2.BuildingPortfolioWebsiteUsingReact/PortfolioWebsite/src/App.jsx
@@ -10,18 +10,19 @@ function App(){
     {
       title: "Project One",
       description: "A web application built using React and Java",
-      link = "#",
+      link :"#",
     },
     {
       title: "Project Two",
       description: "A eCommerce platform built using React and Java",
-      link = "#",
-    }
+      link :"#",
+    },
 
   ]
   
   return(
     <div className='App'>
+      {/* Header Section */}
       <header className='header'>
         <h1>{name}</h1>
         <p>{profession}</p>
@@ -32,6 +33,42 @@ function App(){
         </nav>
         
       </header>
+
+
+      {/* About Section */}
+      <section id='about' className='about-section'>
+        <h2>About Me</h2>
+        <p>Hello! I am {name}, a passionate {profession}. I love web Applications that solves real world user problems.</p>
+      </section>
+
+
+      {/* Project Section */}
+      <section id='projects' className='projects-section'>
+        <h2>Projects</h2>
+        <div className='project-list'>
+          {projects.map((project, index) => (
+            <div key={index} className='project-item'>
+              <h3>{project.title}</h3>
+              <h3>{project.description}</h3>
+            </div>
+          ))}
+        </div>
+      
+      </section>
+
+      {/* Contact Section */}
+      <section id='contact' className='contact-section'>
+          <h2>Contact Me</h2>
+          <p>If you would like to get in touch, feel free to email me at <a href="mailto:johndoe@example.com">johndoe@example.com</a></p>
+
+      </section>
+
+      {/* Footer Section */}
+      <section className='footer'>
+          <p>2026. All right Reserved</p>
+      </section>
     </div>
   );
 }
+
+export default App;
